b1dae5d3a07af8ecec0ee4471fa0b0cb84936cf8
zsebastian1 <44416197+zsebastian1@users.noreply.github.com>
2019-08-12 15:29:28 -0400

    Add files via upload

---
+++ Assignment4_1.java
@@ -0,0 +1,17 @@
+package ACADAssignments;
+
+import testPackage.testMethod;
+
+//import java.testPackage;
+
+public class Assignment4_1 extends testMethod{
+
+	//Assignment4_1 a41 = new Assignment4_1();
+	public static void main(String[] args) {
+		Assignment4_1 a41 = new Assignment4_1();
+		
+		a41.testMethod1();
+		
+	}
+
+}
---
+++ testMethod.java
@@ -0,0 +1,13 @@
+package testPackage;
+
+public class testMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	protected void testMethod1() {
+		System.out.println("This will print in another package!");
+	}
+
+}
